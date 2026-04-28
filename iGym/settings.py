5b1b87873915413182c8f51a6d58da4fad8a5fbc
Mohamed Montaser <info.mmontaser@gmail.com>
2026-04-28 21:50:42 +0300

    create project apps

--- iGym/settings.py
+++ iGym/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'checkins',
+    'customers',
+    'employees',
+    'users'
 ]
 
 MIDDLEWARE = [
